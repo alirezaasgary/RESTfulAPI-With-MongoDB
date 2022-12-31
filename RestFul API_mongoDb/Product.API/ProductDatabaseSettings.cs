94652cd96fc510152918daa91dc395aa16d309c8
alireza asgary <asgary_alireza@hotmail.com>
2023-01-01 01:35:13 +0300

    initial update

---
+++ RestFul API_mongoDb/Product.API/ProductDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Product.API
+{
+    public class ProductDatabaseSettings
+    {
+
+            public string ConnectionString { get; set; } = null!;
+
+            public string DatabaseName { get; set; } = null!;
+
+            public string CollectionName { get; set; } = null!;
+        
+    }
+}
